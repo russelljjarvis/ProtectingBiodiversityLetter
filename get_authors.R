b0fb7648daa5156bc1d5bbbe6e6c3d1da5a52056
Dom Bennett <dominic.john.bennett@gmail.com>
2017-12-03 21:18:23 +0100

    Added explanation to pdf

--- get_authors.R
+++ get_authors.R
@@ -202,7 +202,6 @@ sum(tbl[['Scientist']] == 'Yes')
 tbl <- tbl[order(tbl[['Affiliation']]),]
 tbl <- tbl[order(tbl[['Country']]),]
 tbl <- tbl[order(tbl[['LastName']]),]
-tbl$Signed <- NULL
 write.table(tbl, file='petition_authors.tsv', sep='\t',
             row.names=FALSE)
 write.table(all_cntrs, file='petition_authors_country.tsv', sep='\t',
